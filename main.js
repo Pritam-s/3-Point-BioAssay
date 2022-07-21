d33c544b2ea079bb412a693bd8ead94a1065cc0b
Pritam Singh <singhpritam1704@gmail.com>
2022-07-21 12:47:55 +0530

    Initial Commit - Splash Screen.

---
+++ main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const windowStateKeeper = require('electron-window-state');
+let win;
+
+function createWindow() {
+  let mainWindowState = windowStateKeeper({
+    defaultWidth: 800,
+    defaultHeight: 600,
+  });
+
+
+  win = new BrowserWindow({
+    'x': mainWindowState.x,
+    'y': mainWindowState.y,
+    'width': mainWindowState.width,
+    'height': mainWindowState.height,
+    'backgroundColor': '#EFEFEF',
+
+
+    
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  win.loadFile('index.html')
+  //   win.webContents.openDevTools() // open dev tools
+  mainWindowState.manage(win); // manage window state
+}
+
+app.on('ready', createWindow) // when the app is ready, create the window
